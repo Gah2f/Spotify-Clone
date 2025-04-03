aaf0aef01ff79e7fe3e87a0c97d535fd58f08f4d
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2025-04-03 17:28:35 +0300

    All Components used

---
+++ src/Components/SideBar.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import {assets} from '../assets/frontend-assets/assets'
+function SideBar() {
+  return (
+    <div className='w-[25%] h-full p-2 flex flex-col gap-2 text-white hidden lg:flex'>
+        <div className='bg-[#121212] h-[15%] rounded flex flex-col justify-around '>
+            <div className='flex items-center gap-3 pl-8 cursor-pointer'>
+                <img className='w-6' src={assets.home_icon} alt="Home Icon" />
+                <p className='font-bold'> Home</p>
+            </div>
+            <div className='flex items-center gap-3 pl-8 cursor-pointer'>
+                <img className='w-6' src={assets.search_icon} alt="Search Icon" />
+                <p className='font-bold'> Search </p>
+            </div>
+
+        </div>
+        <div className='bg-[#121212] h-[85%] rounded'>
+            <div className='p-4 flex items-center justify-between'>
+                <div className='flex items-center gap-3'>
+                    <img className='w-8' src={assets.stack_icon} alt="Stack Icon" />
+                    <p className='font-semibold'>Your Library</p>
+                </div>
+                <div className='flex items-center gap-3'>
+                    <img className='w-5' src={assets.arrow_icon} alt="Arrow Icon" />
+                    <img className='w-5 hover:-translate-y-1 duration-500 cursor-pointer' src={assets.plus_icon} alt="Plus Icon" />
+                </div>
+
+            </div>
+            <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
+                <h1>Create your first playlist</h1>
+                <p className='font-light'>It's easy we will help you.</p>
+                <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4 cursor-pointer hover:bg-gray-200'>
+                    Create Playlist
+                </button>
+            </div>
+
+            <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 mt-4'>
+                <h1>Let's find some podcast to follow</h1>
+                <p className='font-light'>We'll keep you update on new episodes.</p>
+                <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4 cursor-pointer hover:bg-gray-200'>
+                    Browse podcasts
+                </button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SideBar
